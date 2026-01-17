6e241f1e4419302a44c512e4926c427c45291fe7
Antonio <antonio3ocon@gmail.com>
2026-01-17 17:08:13 +0100

    Poder elegir el personaje y el enemigo

--- juego.py
+++ juego.py
@@ -1,5 +1,6 @@
 #sleep()
 from time import sleep
+import os
 
 
 class Personaje():
@@ -13,14 +14,14 @@ class Personaje():
     def atacar(self,objetivo):
         self.ataque = True
         self.objetivo = objetivo
-        if self.nombre == 'enemigo_fisico':
+        if self.nombre == 'caballero':
             self.objetivo.vida  = self.objetivo.vida - self.atk
-        elif self.nombre == 'enemigo_especial':
+        elif self.nombre == 'mago':
             self.objetivo.vida = self.objetivo.vida - self.atk_sp
     def __repr__(self):
-        if self.nombre == 'enemigo_fisico':
+        if self.nombre == 'caballero':
             return f'{self.nombre} ha atacado a {self.objetivo.nombre} y le ha hecho {self.atk} de daño dejandolo a {self.objetivo.vida} PS' if self.ataque and self.objetivo else f'Soy {self.nombre}'
-        if self.nombre == 'enemigo_especial':
+        if self.nombre == 'mago':
             return f'{self.nombre} ha atacado a {self.objetivo.nombre} y le ha hecho {self.atk_sp} de daño dejandolo a {self.objetivo.vida} PS' if self.ataque and self.objetivo else f'Soy {self.nombre}'
 
 class Fisico(Personaje):
@@ -30,26 +31,66 @@ class Especial(Personaje):
     atk_sp = Personaje.atk_sp*2
 
 
-enemigo_fisico = Fisico('enemigo_fisico')
-enemigo_especial = Especial('enemigo_especial')
+caballero_enemigo = Fisico('caballero_enemigo')
+brujo = Especial('brujo')
 caballero = Fisico('caballero')
 mago = Especial('mago')
 
+while True:
+    try:
+        print('1. Caballero')
+        print('2. Mago')
+        elegir = int(input('Elige uno de los dos personajes jugables (1/2): '))
+
+        if elegir==1:
+            elegir=caballero
+            break
+        elif elegir==2:
+            elegir=mago
+            break
+        else:
+            print('Solo puedes elegir 1 o 2 idiota')
+            sleep(1)
+            os.system('cls')
+    except:
+        print('Solo puedes elegir 1 o 2 idiota')
+        sleep(1)
+        os.system('cls')
+
 print('----------------------------')
-print(enemigo_especial)
 
-enemigo_especial.atacar(caballero)
+while True:
+    try:
+        print('1. Caballero Enemigo')
+        print('2. Brujo')
+        elegir2 = int(input('Elige uno de los dos enemigos para enfrentarte (1/2): '))
 
-print(enemigo_especial)
+        if elegir2==1:
+            elegir2=caballero_enemigo
+            break
+        elif elegir2==2:
+            elegir2=brujo
+            break
+        else:
+            print('Solo puedes elegir 1 o 2 idiota')
+            sleep(1)
+            os.system('cls')
+    except:
+        print('Solo puedes elegir 1 o 2 idiota')
+        sleep(1)
+        os.system('cls')
+
+print('')
+print(f'Eres {elegir.nombre} y te vas a enfrentar a {elegir2.nombre} que tiene {elegir2.vida}')
+
+print('      -------------         ')
 print('----------------------------')
-
-enemigo_fisico = Fisico('enemigo_fisico')
-enemigo_especial = Especial('enemigo_especial')
-caballero = Fisico('caballero')
-mago = Especial('mago')
-
-print(enemigo_fisico)
-
-enemigo_fisico.atacar(caballero)
-
-print(enemigo_fisico)
+while True:
+    if elegir2.vida>0:
+        print(elegir)
+        elegir.atacar(elegir2)
+    else:
+        print(f'{elegir.nombre} ha derrotado a {elegir2.nombre}')
+        break
+    print('----------------------------')
+print('      -------------         ')
